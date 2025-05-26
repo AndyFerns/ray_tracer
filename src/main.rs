312b3ecabd5d4378adb9a677a3786300755a72a0
AndyFerns <write2andrew.fernandes@gmail.com>
2025-05-26 17:26:46 +0530

    changed output format

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod camera;
 use vec3::Vec3;
 use sphere::Sphere;
 use camera::Camera;
+use image::{RgbImage, Rgb};
 
 fn ray_color(ray: &ray::Ray, sphere: &Sphere) -> Vec3 {
     if let Some(hit) = sphere.hit(ray, 0.001, f64::INFINITY) {
@@ -19,25 +20,31 @@ fn ray_color(ray: &ray::Ray, sphere: &Sphere) -> Vec3 {
 }
 
 fn main() {
-    let width = 200;
-    let height = 100;
+    let width = 800;
+    let height = 400;
     let camera = Camera::new();
     let sphere = Sphere {
         center: Vec3(0.0, 0.0, -1.0),
         radius: 0.5,
     };
 
-    println!("P3\n{} {}\n255", width, height);
-    for j in (0..height).rev() {
+    let mut img = RgbImage::new(width, height);
+
+    for j in 0..height {
         for i in 0..width {
             let u = i as f64 / width as f64;
-            let v = j as f64 / height as f64;
+            let v = (height - j - 1) as f64 / height as f64; // flip vertically
             let r = camera.get_ray(u, v);
             let color = ray_color(&r, &sphere);
-            let ir = (255.99 * color.0) as i32;
-            let ig = (255.99 * color.1) as i32;
-            let ib = (255.99 * color.2) as i32;
-            println!("{} {} {}", ir, ig, ib);
+            let pixel = Rgb([
+                (255.99 * color.0) as u8,
+                (255.99 * color.1) as u8,
+                (255.99 * color.2) as u8,
+            ]);
+            img.put_pixel(i, j, pixel);
         }
     }
+
+    img.save("output.png").unwrap();
+    println!("Image saved to output.png");
 }
